986eef166c94ee16767d997b17a8cb64157546b9
Camilo Vera Bezmalinovic <camo@axiomzen.co>
2016-07-21 10:46:13 -0400

    complete game and transitions

---
+++ sesiones/05/TicTacToe/TicTacToe/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  TicTacToe
+//
+//  Created by Camilo Vera Bezmalinovic on 7/18/16.
+//  Copyright © 2016 Camilo Vera Bezmalinovic. All rights reserved.
+//
+
+
+// github.com/ios-love/duoc-workshop
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
---
+++ sesiones/05/TicTacToe/TicTacToe/Mark.swift
@@ -0,0 +1,12 @@
+//
+//  Mark.swift
+//  TicTacToe
+//
+//  Created by Camilo Vera Bezmalinovic on 7/18/16.
+//  Copyright © 2016 Camilo Vera Bezmalinovic. All rights reserved.
+//
+
+enum Mark: String {
+    case X
+    case O
+}
